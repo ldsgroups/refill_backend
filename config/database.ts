c438dd0b20b70a674014b5adf6a9fb5cdea53b19
Darius Kassi <kassidarius@gmail.com>
2024-02-11 10:37:23 +0000

    Initial commit with all deps needed

---
+++ config/database.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from '@adonisjs/lucid'
+
+const dbConfig = defineConfig({
+  connection: 'postgres',
+  connections: {
+    postgres: {
+      client: 'pg',
+      connection: {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT!),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+      },
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+    },
+  },
+})
+
+export default dbConfig
